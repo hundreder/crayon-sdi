c8a4c24ad6f4fbf8113625e07a7fc0a7f8cd66cf
srdjan.majstorovic <srdjan.majstorovic@aspendental.com>
2025-03-30 19:09:52 +0200

    moved private functions to local functions

--- coding-exercise/Crayon/Crayon.API/Services/CustomerAccountsService.cs
+++ coding-exercise/Crayon/Crayon.API/Services/CustomerAccountsService.cs
@@ -9,8 +9,8 @@ public interface ICustomerAccountsService
 {
     Task<Customer> GetCustomerAndAccounts(int customerId, CancellationToken ct);
     Task<List<Subscription>> GetSubscriptions(int customerId, int accountId, CancellationToken ct);
-    Task<Either<ChangeLicenceCountError, Unit>> ChangeLicenceCount(int customerId, int licenceId, int newLicenceCount, CancellationToken ct);
     Task<Either<CancelSubscriptionError, Unit>> CancelSubscription(int customerId, int subscriptionId, CancellationToken ct);
+    Task<Either<ChangeLicenceCountError, Unit>> ChangeLicenceCount(int customerId, int licenceId, int newLicenceCount, CancellationToken ct);
 }
 
 public class CustomerAccountsService(CrayonDbContext dbContext) : ICustomerAccountsService
@@ -26,7 +26,34 @@ public class CustomerAccountsService(CrayonDbContext dbContext) : ICustomerAccou
             .SelectMany(acc => acc.Subscriptions)
             .ToListAsync(ct);
 
+    public async Task<Either<CancelSubscriptionError, Unit>> CancelSubscription(int customerId, int subscriptionId, CancellationToken ct)
+    {
+        var subscription = await dbContext.Subscriptions
+            .AsSingleQuery()
+            .Include(s => s.Account)
+            .SingleOrDefaultAsync(sub => sub.Id == subscriptionId, cancellationToken: ct);
 
+        if (subscription is null)
+            return CancelSubscriptionError.SubscriptionDoesNotExist;
+
+        if (!SubscriptionBelongsToCustomer(customerId, subscription))
+            return CancelSubscriptionError.SubscriptionDoesNotExist;
+        
+        // Could have been implemented to be idempotent, then this check wouldn't be performed
+        if (subscription.Status == SubscriptionStatus.Cancelled) 
+            return CancelSubscriptionError.SubscriptionAlreadyCanceled; 
+
+        subscription.Status = SubscriptionStatus.Cancelled;
+        subscription.UpdatedAt = DateTimeOffset.UtcNow;
+        // if there is any side work to do, do it here. like calling ccp to cancel licences. Then some intermediate state od subscription should be added.
+        
+        await dbContext.SaveChangesAsync(ct);
+
+        return Unit.Default;
+        
+        bool SubscriptionBelongsToCustomer(int customerId, Subscription subscription) => subscription.Account.CustomerId == customerId;
+    }
+    
     public async Task<Either<ChangeLicenceCountError, Unit>> ChangeLicenceCount(int customerId, int licenceId, int newLicenceCount, CancellationToken ct)
     {
         var licence = await dbContext.Licences
@@ -50,35 +77,10 @@ public class CustomerAccountsService(CrayonDbContext dbContext) : ICustomerAccou
         await dbContext.SaveChangesAsync(ct);
 
         return Unit.Default;
-    }
-
-    public async Task<Either<CancelSubscriptionError, Unit>> CancelSubscription(int customerId, int subscriptionId, CancellationToken ct)
-    {
-        var subscription = await dbContext.Subscriptions
-            .AsSingleQuery()
-            .Include(s => s.Account)
-            .SingleOrDefaultAsync(sub => sub.Id == subscriptionId, cancellationToken: ct);
-
-        if (subscription is null)
-            return CancelSubscriptionError.SubscriptionDoesNotExist;
-
-        if (!SubscriptionBelongsToCustomer(customerId, subscription))
-            return CancelSubscriptionError.SubscriptionDoesNotExist;
-
-        if (subscription.Status == SubscriptionStatus.Cancelled)
-            return CancelSubscriptionError.SubscriptionAlreadyCanceled;
-
-        subscription.Status = SubscriptionStatus.Cancelled;
-
-        subscription.UpdatedAt = DateTimeOffset.UtcNow;
         
-        await dbContext.SaveChangesAsync(ct);
-
-        return Unit.Default;
+        bool LicenceBelongsToCustomer(int customerId, Licence licence) => licence.Subscription.Account.CustomerId == customerId;
     }
-
-    private bool SubscriptionBelongsToCustomer(int customerId, Subscription subscription) => subscription.Account.CustomerId == customerId;
-    private bool LicenceBelongsToCustomer(int customerId, Licence licence) => licence.Subscription.Account.CustomerId == customerId;
+    
 }
 
 public enum ChangeLicenceCountError
